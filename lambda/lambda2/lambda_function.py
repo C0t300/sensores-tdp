55def100f4c7f487e12781a058ed822283a46992
C0t300 <jose@runin.cl>
2023-07-03 17:40:10 -0400

    lambda2 working

--- lambda/lambda2/lambda_function.py
+++ lambda/lambda2/lambda_function.py
@@ -1,6 +1,27 @@
-# Lambda 1 does insertions into the DBs
+# Lambda 2 does select on the db
 import pg8000.native
 import json
+
+r = [
+        3,
+        "2023-07-02 16:35:54.568529",
+        "luz",
+        56.0,
+        False
+    ]
+
+def map_to_dict(rows):
+
+    keys = ['id', 'fecha', 'sensor', 'value', 'error']
+
+    d = []
+    for i in range(len(rows)):
+        d2 = {}
+        for j in range(len(rows[i])):
+            d2[keys[j]] = rows[i][j]
+        d.append(d2)
+    return d
+
 
 def lambda_handler(event, context):
 
@@ -28,9 +49,9 @@ def lambda_handler(event, context):
     with pg8000.native.Connection(host="database-2.c1doqm3wbuwt.us-east-2.rds.amazonaws.com", user="postgres", password="postgres", database="main") as conn:
 
         response = conn.run("select * from logs where sensor = :sensor", sensor=body['sensor'])
-        
+        response = map_to_dict(response)
         
     return {
         'statusCode': 200,
-        'body': json.dumps(response)
+        'body': json.dumps(response, default=str)
     }
